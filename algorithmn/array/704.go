62a13c2bb90b0dd2b297f734f7aac2127a974311
zhang.hao <zhang.hao@bytedance.com>
2023-11-12 22:35:40 +0800

    feat: add 704

--- algorithmn/array/704.go
+++ algorithmn/array/704.go
@@ -1,1 +1,27 @@
 package array
+
+func search(nums []int, target int) int {
+	start := 0
+	end := len(nums) - 1
+
+	// [start, end]
+	for start <= end {
+		middle := (start + end) / 2
+		if target == nums[middle] {
+			return middle
+		} else if target > nums[middle] {
+			start = middle + 1
+		} else {
+			end = middle - 1
+		}
+
+		if start == end {
+			if nums[start] == target {
+				return start
+			} else {
+				return -1
+			}
+		}
+	}
+	return -1
+}
