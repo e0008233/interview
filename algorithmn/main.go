978af4bfd24a3ab2aba44f14fe684d6ba6b849c9
zhang.hao <zhang.hao@bytedance.com>
2024-03-19 11:14:36 +0800

    feat: add question

--- algorithmn/main.go
+++ algorithmn/main.go
@@ -2,30 +2,10 @@ package main
 
 import (
 	"fmt"
-	"interview/algorithmn/interview"
 )
 
 func main() {
-	accountList := make([]interview.Account, 0)
-	account1 := interview.Account{
-		"a",
-		70,
-	}
-
-	account2 := interview.Account{
-		"b",
-		70,
-	}
-
-	account3 := interview.Account{
-		"a",
-		200,
-	}
-
-	accountList = append(accountList, account1)
-	accountList = append(accountList, account2)
-	accountList = append(accountList, account3)
-
-	fmt.Println(interview.MakeTransfer(accountList, 100))
+	bytess := []byte{}
+	fmt.Println(string(bytess))
 
 }
